c5e226207f731d2dea2414e1f737a8891b62f1a1
mfaizudd <mfaizudd@gmail.com>
2022-06-16 10:21:52 +0700

    fix: add response model

--- src/models.rs
+++ src/models.rs
@@ -1,6 +1,12 @@
-use serde::{Serialize, Deserialize};
-use uuid::Uuid;
 use super::schema::users;
+use serde::{Deserialize, Serialize};
+use uuid::Uuid;
+
+#[derive(Debug, Serialize)]
+pub struct Response<T: Serialize> {
+    pub status: u16,
+    pub data: T,
+}
 
 #[derive(Debug, Serialize, Deserialize, Queryable, Identifiable, AsChangeset)]
 pub struct User {
@@ -9,15 +15,15 @@ pub struct User {
     pub email: String,
     pub password: String,
     pub created_at: chrono::NaiveDateTime,
-    pub updated_at: chrono::NaiveDateTime
+    pub updated_at: chrono::NaiveDateTime,
 }
 
 #[derive(Insertable, AsChangeset)]
-#[table_name="users"]
+#[table_name = "users"]
 pub struct NewUser<'a> {
     pub full_name: &'a str,
     pub email: &'a str,
     pub password: &'a str,
     pub created_at: chrono::NaiveDateTime,
-    pub updated_at: chrono::NaiveDateTime
+    pub updated_at: chrono::NaiveDateTime,
 }
