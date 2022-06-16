f6009e182c85a44aad34b2d21af257d1cb683338
mfaizudd <mfaizudd@gmail.com>
2022-06-16 09:50:18 +0700

    fix: app data error

--- src/main.rs
+++ src/main.rs
@@ -1,21 +1,20 @@
-extern crate pos_rs;
 extern crate diesel;
+extern crate pos_rs;
 
 use actix_web::middleware::Logger;
-use actix_web::{App, HttpServer};
+use actix_web::{web, App, HttpServer};
 use env_logger::Env;
-use pos_rs::handlers;
 use pos_rs::db;
+use pos_rs::handlers;
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
-
     env_logger::init_from_env(Env::default().default_filter_or("info"));
     let pool = db::create_connection_pool();
-    
+
     HttpServer::new(move || {
         App::new()
-            .app_data(pool.clone())
+            .app_data(web::Data::new(pool.clone()))
             .wrap(Logger::default())
             .configure(handlers::configuration)
     })
