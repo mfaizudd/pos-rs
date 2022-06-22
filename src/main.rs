ca283d4ae597b965c4c345d2d18679ccf630769f
mfaizudd <mfaizudd@gmail.com>
2022-06-22 12:42:26 +0700

    style: remove unused uses

--- src/main.rs
+++ src/main.rs
@@ -2,24 +2,24 @@ extern crate diesel;
 extern crate pos_rs;
 
 use std::env;
-use std::sync::Arc;
 
 use actix_session::SessionMiddleware;
 use actix_session::storage::CookieSessionStore;
+use actix_web::{App, HttpServer, web};
 use actix_web::cookie::Key;
 use actix_web::middleware::Logger;
-use actix_web::{web, App, HttpServer};
 use dotenv::dotenv;
 use env_logger::Env;
 use secrecy::{ExposeSecret, Secret};
-use pos_rs::{db, AppState};
+
+use pos_rs::{AppState, db};
 use pos_rs::handlers;
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
     dotenv().ok();
     env_logger::init_from_env(Env::default().default_filter_or("info"));
-    
+
     HttpServer::new(move || {
         let secret = env::var("SECRET").expect("Secret must be set");
         let pool = db::create_connection_pool();
@@ -38,7 +38,7 @@ async fn main() -> std::io::Result<()> {
             .wrap(session)
             .configure(handlers::configuration)
     })
-    .bind(("127.0.0.1", 8000))?
-    .run()
-    .await
+        .bind(("127.0.0.1", 8000))?
+        .run()
+        .await
 }
